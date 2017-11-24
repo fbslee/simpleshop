d0fe13768041748b170b35f3cfe6fd123a0959c1
Felipe Lee <fbslee@gmail.com>
2017-11-24 11:18:08 -0800

    fixing

--- server/server.js
+++ server/server.js
@@ -20,12 +20,12 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 // app.use(forceSSL());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-app.use(express.static(__dirname + '/../dist'));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+app.use(express.static(path.join(__dirname, './dist')));
 app.use('/api', router);
 
 var clients = {};
