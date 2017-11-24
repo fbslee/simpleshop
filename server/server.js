5e0f3562e554d9ffb5fb7af5a70ba877f2a04681
Felipe Lee <fbslee@gmail.com>
2017-11-24 11:15:11 -0800

    fixing issues

--- server/server.js
+++ server/server.js
@@ -25,8 +25,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use('/api', router);
 app.use(express.static(__dirname + '/../dist'));
+app.use('/api', router);
 
 var clients = {};
 
