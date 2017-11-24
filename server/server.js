daa1ad98548ebcda097539e731409c42258fe0d8
Felipe Lee <fbslee@gmail.com>
2017-11-24 00:45:24 -0800

    fixing issues

--- server/server.js
+++ server/server.js
@@ -5,21 +5,21 @@ var bodyParser = require('body-parser');
 var router = require('./routes/routes');
 var app = express();
 var server = require('http').createServer(app);
-var forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
+// var forceSSL = function() {
+//     return function (req, res, next) {
+//       if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect(
+//          ['https://', req.get('Host'), req.url].join('')
+//         );
+//       }
+//       next();
+//     }
+//   }
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(forceSSL());
+// app.use(forceSSL());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
 });
 app.use('/api', router);
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/../dist/index.html'));
   });
 
 var clients = {};
